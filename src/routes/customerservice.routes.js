8509aa7a0e2e601f7a10147bbb72699837047346
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-09-05 10:44:12 +0700

    Renaming Files

--- src/routes/customerservice.routes.js
+++ src/routes/customerservice.routes.js
@@ -1,16 +1,16 @@
 const { authJwt } = require("../middleware");
-const customerservice = require("../controllers/CustomerService.controller.js");
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+const customerservice = require("../controllers/customerService.controllers.js");
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
-    app.get(
+  app.get(
     "/api/test/cs",
-    [authJwt.verifyToken, authJwt.isAdminCS , authJwt.isActivated],
+    [authJwt.verifyToken, authJwt.isAdminCS, authJwt.isActivated],
     customerservice.adminCSBoard
-    );
+  );
 };
