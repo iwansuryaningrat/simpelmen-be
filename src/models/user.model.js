7b4eb5d8715b5a6fbe64365596823b5cc6ac6448
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-09-07 13:28:56 +0700

    Update User Model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,23 +1,31 @@
 const userModel = (sequelize, Sequelize) => {
-  const User = sequelize.define("users", {
-    username: {
-      type: Sequelize.STRING,
+  const User = sequelize.define(
+    "users",
+    {
+      username: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        defaultValue: "user",
+      },
     },
-    email: {
-      type: Sequelize.STRING,
-    },
-    password: {
-      type: Sequelize.STRING,
-    },
-    active: {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false,
-    },
-    role: {
-      type: Sequelize.STRING,
-      defaultValue: "user",
-    },
-  });
+    {
+      timestamps: true, // Enable timestamps
+      createdAt: true, // Don't create createdAt
+      updatedAt: true, // Don't create updatedAt
+    }
+  );
 
   return User;
 };
