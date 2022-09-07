a1c50ba1e119795cfc269508ea4d659a554ee43c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-09-07 10:05:34 +0700

    Edit Cashier Router
    Edit to module type

--- src/routes/cashier.routes.js
+++ src/routes/cashier.routes.js
@@ -1,16 +1,19 @@
-const { authJwt } = require("../middleware");
-const cashier = require("../controllers/Cashier.controller.js");
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-    app.get(
-    "/api/test/kasir",
-    [authJwt.verifyToken, authJwt.isAdminKasir , authJwt.isActivated],
-    cashier.adminKasirBoard
-    );
+import {
+  verifyToken,
+  isAdmin,
+  isAdminKasir,
+  isAdminCS,
+  isActivated,
+} from "../controllers/auth.controllers.js";
+import adminKasirBoard from "../controllers/cashier.controllers.js";
+import { Express } from "express";
+const router = Express.Router();
+import headers from "../services/headers.js";
+
+const cashierRouter = (app) => {
+  app.use(headers);
+
+  router.get("/kasir", verifyToken, isActivated, isAdminKasir, adminKasirBoard);
+
+  app.use("/api/test", router);
 };
