560ded469cbf010cdd146087236196d3f447b0f0
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-07 14:26:38 +0700

    Create Roles Model

---
+++ src/models/roles.model.js
@@ -0,0 +1,24 @@
+const roles = (sequelize, Sequelize) => {
+  const Roles = sequelize.define(
+    "roles",
+    {
+      role_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      role_name: {
+        type: Sequelize.STRING,
+      },
+    },
+    {
+      timestamps: true, // Enable timestamps
+      createdAt: true, // Create createdAt
+      updatedAt: true, // Create updatedAt
+    }
+  );
+
+  return Roles;
+};
+
+export default roles;
