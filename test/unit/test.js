8426ddec4bcbbbe32525d087136b8c44e184895c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-09 02:12:46 +0700

    Create Test File

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,1 +1,17 @@
+// import jwt from "jsonwebtoken";
+// import * as dotenv from "dotenv";
 
+// dotenv.config();
+
+// const secret = "secret";
+
+// const token = jwt.sign({ id: 1 }, secret, { expiresIn: 60 * 60 });
+
+// console.log(token);
+
+// const decoded = jwt.verify(token, secret);
+
+// console.log(decoded);
+
+import "dotenv/config";
+console.log(process.env.JWT_SECRET);
