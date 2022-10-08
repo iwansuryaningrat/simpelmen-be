099f353baab37795cd6ce020d46eb4cb09fd213b
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-09 01:09:02 +0700

    Update Auth Middlewares

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -25,12 +25,20 @@ const isLogin = (req, res, next) => {
 
 const isActivated = (req, res, next) => {
   const token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(401).send({
+      message: "No token, authorization denied",
+    });
+  }
+
   jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-    if (!decoded.active) {
+    if (!decoded.user_status || !decoded.user_verify) {
       return res.status(401).send({
         message: "Email not activated or Token expired!",
       });
     }
+
     next();
   });
 };
