57e8eeaae4e987ced1f63307761a7fa2d02c36e2
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-09 00:56:40 +0700

    Create Email Checker Middlewares

---
+++ src/middlewares/accountChecker.middlewares.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import db from "../models/index.js";
+const Users = db.users;
+
+const checkEmail = (req, res, next) => {
+  const email = req.body.email;
+
+  Users.findOne({
+    where: {
+      email: email,
+    },
+  })
+    .then((data) => {
+      if (data) {
+        return res.status(406).send({
+          message: "Email already exists",
+        });
+      }
+
+      next();
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving users.",
+      });
+    });
+};
