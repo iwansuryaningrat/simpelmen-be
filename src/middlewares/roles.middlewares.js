a9dec6d8d5c7c87a8384b9923f868e8baf61df4c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-09 02:12:15 +0700

    Fixing JWT Issue at Roles Middlewares

--- src/middlewares/roles.middlewares.js
+++ src/middlewares/roles.middlewares.js
@@ -3,196 +3,75 @@ import db from "../models/index.js";
 const Users = db.users;
 
 const isSuperAdmin = (req, res, next) => {
-  jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET_KEY,
-    (err, decoded) => {
-      User.findOne({
-        where: {
-          user_id: decoded.id,
-          role: decoded.role,
-        },
-      }).then((user) => {
-        if (user.role === "super_admin") {
-          next();
-        } else {
-          return res.status(401).send({
-            message: "Unauthorized!",
-          });
-        }
-      });
-    }
-  );
-};
+  const email = req.email;
+  const role = req.role;
 
-const isAdminCashier = (req, res, next) => {
-  jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET_KEY,
-    (err, decoded) => {
-      User.findOne({
-        where: {
-          user_id: decoded.id,
-          role: decoded.role,
-        },
-      }).then((user) => {
-        if (user.role === "cashier") {
-          next();
-        } else {
-          return res.status(401).send({
-            message: "Unauthorized!",
-          });
-        }
-      });
-    }
-  );
+  if (role === 1) {
+    next();
+  }
 };
 
 const isAdminCS = (req, res, next) => {
-  jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET_KEY,
-    (err, decoded) => {
-      User.findOne({
-        where: {
-          user_id: decoded.id,
-          role: decoded.role,
-        },
-      }).then((user) => {
-        if (user.role === "customer_service") {
-          next();
-        } else {
-          return res.status(401).send({
-            message: "Unauthorized!",
-          });
-        }
-      });
-    }
-  );
+  const email = req.email;
+  const role = req.role;
+
+  if (role === 2) {
+    next();
+  }
 };
 
-const isAdminWarehouse = (req, res, next) => {
-  jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET_KEY,
-    (err, decoded) => {
-      User.findOne({
-        where: {
-          user_id: decoded.id,
-          role: decoded.role,
-        },
-      }).then((user) => {
-        if (user.role === "admin_warehouse") {
-          next();
-        } else {
-          return res.status(401).send({
-            message: "Unauthorized!",
-          });
-        }
-      });
-    }
-  );
+const isAdminCashier = (req, res, next) => {
+  const email = req.email;
+  const role = req.role;
+
+  if (role === 3) {
+    next();
+  }
 };
 
 const isAdminDesign = (req, res, next) => {
-  jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET_KEY,
-    (err, decoded) => {
-      User.findOne({
-        where: {
-          user_id: decoded.id,
-          role: decoded.role,
-        },
-      }).then((user) => {
-        if (user.role === "admin_design") {
-          next();
-        } else {
-          return res.status(401).send({
-            message: "Unauthorized!",
-          });
-        }
-      });
-    }
-  );
+  const email = req.email;
+  const role = req.role;
+
+  if (role === 4) {
+    next();
+  }
 };
 
-const isUser = (req, res, next) => {
-  jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET_KEY,
-    (err, decoded) => {
-      User.findOne({
-        where: {
-          user_id: decoded.id,
-          role: decoded.role,
-        },
-      }).then((user) => {
-        if (user.role === "user") {
-          next();
-        } else {
-          return res.status(401).send({
-            message: "Unauthorized!",
-          });
-        }
-      });
-    }
-  );
+const isAdminWarehouse = (req, res, next) => {
+  const email = req.email;
+  const role = req.role;
+
+  if (role === 5) {
+    next();
+  }
 };
 
 const isAdminProduction = (req, res, next) => {
-  jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET_KEY,
-    (err, decoded) => {
-      User.findOne({
-        where: {
-          user_id: decoded.id,
-          role: decoded.role,
-        },
-      }).then((user) => {
-        if (user.role === "admin_production") {
-          next();
-        } else {
-          return res.status(401).send({
-            message: "Unauthorized!",
-          });
-        }
-      });
-    }
-  );
+  const email = req.email;
+  const role = req.role;
+
+  if (role === 6) {
+    next();
+  }
 };
 
 const isAdminTu = (req, res, next) => {
-  jwt.verify(
-    req.headers["x-access-token"],
-    process.env.SECRET_KEY,
-    (err, decoded) => {
-      User.findOne({
-        where: {
-          user_id: decoded.id,
-          role: decoded.role,
-        },
-      }).then((user) => {
-        if (user.role === "admin_tu") {
-          next();
-        } else {
-          return res.status(401).send({
-            message: "Unauthorized!",
-          });
-        }
-      });
-    }
-  );
+  const email = req.email;
+  const role = req.role;
+
+  if (role === 7) {
+    next();
+  }
 };
 
-export {
-  isSuperAdmin,
-  isAdminCashier,
-  isAdminCS,
-  isAdminWarehouse,
-  isAdminDesign,
-  isUser,
-  isAdminProduction,
-  isAdminTu,
+const isUser = (req, res, next) => {
+  const email = req.email;
+  const role = req.role;
+
+  if (role === 8) {
+    next();
+  }
 };
+
+export { isSuperAdmin };
