c3620d2d0fd9db8521c463f54bb0be160c4e7b4f
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-08 11:40:14 +0700

    sd3783jhrejrtkre7393y36gf6

--- src/controllers/users/administration.controller.js
+++ src/controllers/users/administration.controller.js
@@ -39,58 +39,57 @@ const updateProfile = (req, res) => {
 };
 const AdministratorTransaction = (req, res) => {
   Transaction.findAll({
-      where: {
-        status_history_id: {
-          [Op.or]: [5],
-          },
+    where: {
+      status_history_id: {
+        [Op.or]: [5],
       },
-      include: [
+    },
+    include: [
+      {
+        model: User,
+        as: "user",
+        attributes: ["user_id", "username", "email"],
+      },
+      {
+        model: Product,
+        as: "product",
+        include: [
           {
-              model: User,
-              as: "user",
-              attributes: ["user_id", "username", "email"],
+            model: finishing,
+            as: "finishing",
+            attributes: ["finishing_id", "finishing_name"],
           },
           {
-              model: Product,
-              as: "product",
-              include: [
-                  {
-                      model:finishing,
-                      as:"finishing",
-                      attributes: ["finishing_id", "finishing_name"],
-                  },
-                  {
-                      model:Product_detail,
-                      as:"product_detail",
-                      attributes: ["product_detail_id", "product_detail_name"],
-                      include:[
-                          {
-                              model:Material,
-                              as:"material",
-                              attributes: ["material_id", "material_name"],
-                          },
-                          {
-                              model:Product_Category,
-                              as:"product_category",
-                              attributes: ["product_category_id", "product_category_name"],
-                          },
-                          {
-                              model:Size, 
-                              as:"size"
-                          }
-                      ]
-                  },
-              ]
+            model: Product_detail,
+            as: "product_detail",
+            attributes: ["product_detail_id", "product_detail_name"],
+            include: [
+              {
+                model: Material,
+                as: "material",
+                attributes: ["material_id", "material_name"],
+              },
+              {
+                model: Product_Category,
+                as: "product_category",
+                attributes: ["product_category_id", "product_category_name"],
+              },
+              {
+                model: Size,
+                as: "size",
+              },
+            ],
           },
-          {
-              model:Status_Histories,
-              as:"status_histories",
-              attributes: ["status_history_id", "status_history_name"],
-
-          }
-      ],
+        ],
+      },
+      {
+        model: Status_Histories,
+        as: "status_histories",
+        attributes: ["status_history_id", "status_history_name"],
+      },
+    ],
   }).then((transaction) => {
-      res.status(200).send(transaction);
+    res.status(200).send(transaction);
   });
 };
-export { showProfile, updateProfile ,AdministratorTransaction};
+export { showProfile, updateProfile, AdministratorTransaction };
