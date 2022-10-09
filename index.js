1f1765e7b2efd24078c66ad981b6d9360f00c6fe
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-10 00:27:48 +0700

    Edit Database Sync Config
    Force : False

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.use(
 // database
 import db from "./src/models/index.js";
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   console.log("Sync database");
 });
 
