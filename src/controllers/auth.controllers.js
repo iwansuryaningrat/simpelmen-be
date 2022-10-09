f711773c274480f9e23b3bfbccd2fd4e2edf59f6
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-09 21:54:39 +0700

    Create Reset Password
    Not Fix

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -185,145 +185,150 @@ const login = (req, res) => {
     });
 };
 
-// const SendResetPassword = (req, res) => {
-//   const { email } = req.body;
-//   User.findOne({
-//     where: {
-//       email: email,
-//     },
-//   })
-//     .then((user) => {
-//       if (!user) {
-//         return res.status(404).send({ message: "User Not found." });
-//       }
-//       const token = jwt.sign(
-//         {
-//           email: email,
-//         },
-//         process.env.SECRET_KEY,
-//         {
-//           expiresIn: 1800,
-//         }
-//       );
-//       const DOMAIN = process.env.MAILGUN_DOMAIN;
-//       const mg = mailgun({
-//         apiKey: process.env.MAILGUN_API_KEY,
-//         domain: DOMAIN,
-//       });
-//       const data = {
-//         from: "Admin <widiw598@gmail.com>",
-//         to: email,
-//         subject: "Reset Password",
-//         html: `
-//         <html>
-//         <head>
-//         <style>
-//         .container {
-//           width: 100%;
-//           height: 100%;
-//           background-color: orange;
-//           padding: 20px;
-//         }
-//         .card {
-//           width: 400px;
-//           height: 400px;
-//           background-color: white;
-//           margin: 0 auto;
-//           margin-top: 100px;
-//           padding: 20px;
-//           border-radius: 10px;
-//           box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-//         }
-//         .card h1 {
-//           text-align: center;
-//           font-size: 30px;
-//           margin-bottom: 20px;
-//         }
-//         .card p {
-//           text-align: center;
-//           font-size: 20px;
-//           margin-bottom: 20px;
-//         }
-//         .card a {
-//           text-decoration: none;
-//           background-color: #000;
-//           color: #fff;
-//           padding: 10px 20px;
-//           border-radius: 5px;
-//           display: block;
-//           width: 100px;
-//           text-align: center;
-//           margin: 0 auto;
-//         }
-//         </style>
-//         </head>
-//         <body>
-//         <div class="container">
-//         <div class="card">
-//         <h1>Reset Password</h1>
-//         <p>Click the button below to reset your password.</p>
-//         <a href="http://localhost:8080/api/auth/reset/${token}">Reset</a>
-//         </div>
-//         </div>
-//         </body>
-//         </html>
-//         `,
-//       };
-//       mg.messages().send(data, function (error, body) {
-//         if (error) {
-//           return res.json({
-//             error: err.message,
-//           });
-//         }
-//         return res.json({
-//           message: "Email has been sent",
-//         });
-//       });
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: err.message,
-//       });
-//     });
-// };
+const SendResetPassword = (req, res) => {
+  const { email } = req.body;
 
-// const ResetPassword = (req, res) => {
-//   const { token } = req.params;
-//   const { password } = req.body;
-//   if (token) {
-//     jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
-//       if (err) {
-//         res.status(400).send({ message: "Incorrect or Expired link" });
-//       } else {
-//         const { email } = decodedToken;
-//         User.findOne({
-//           where: {
-//             email: email,
-//           },
-//         })
-//           .then((user) => {
-//             if (!user) {
-//               return res.status(404).send({ message: "User Not found." });
-//             }
-//             user
-//               .update({
-//                 password: bcrypt.hashSync(password, 8),
-//               })
-//               .then(() => {
-//                 res.status(200).send({ message: "Password updated" });
-//               })
-//               .catch((err) => {
-//                 res.status(500).send({ message: err.message });
-//               });
-//           })
-//           .catch((err) => {
-//             res.status(500).send({ message: err.message });
-//           });
-//       }
-//     });
-//   } else {
-//     res.status(400).send({ message: "Something went wrong" });
-//   }
-// };
+  Users.findOne({
+    where: {
+      user_email: email,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
 
-export { signup, activate, login };
+      const token = jwt.sign(
+        {
+          email: email,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: 1800,
+        }
+      );
+
+      const DOMAIN = process.env.MAILGUN_DOMAIN;
+      const mg = mailgun({
+        apiKey: process.env.MAILGUN_API_KEY,
+        domain: DOMAIN,
+      });
+      const data = {
+        from: "Admin <widiw598@gmail.com>",
+        to: email,
+        subject: "Reset Password",
+        html: `
+        <html>
+        <head>
+        <style>
+        .container {
+          width: 100%;
+          height: 100%;
+          background-color: orange;
+          padding: 20px;
+        }
+        .card {
+          width: 400px;
+          height: 400px;
+          background-color: white;
+          margin: 0 auto;
+          margin-top: 100px;
+          padding: 20px;
+          border-radius: 10px;
+          box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        .card h1 {
+          text-align: center;
+          font-size: 30px;
+          margin-bottom: 20px;
+        }
+        .card p {
+          text-align: center;
+          font-size: 20px;
+          margin-bottom: 20px;
+        }
+        .card a {
+          text-decoration: none;
+          background-color: #000;
+          color: #fff;
+          padding: 10px 20px;
+          border-radius: 5px;
+          display: block;
+          width: 100px;
+          text-align: center;
+          margin: 0 auto;
+        }
+        </style>
+        </head>
+        <body>
+        <div class="container">
+        <div class="card">
+        <h1>Reset Password</h1>
+        <p>Click the button below to reset your password.</p>
+        <a href="http://localhost:8080/api/auth/reset/${token}">Reset</a>
+        </div>
+        </div>
+        </body>
+        </html>
+        `,
+      };
+
+      mg.messages().send(data, function (error, body) {
+        if (error) {
+          return res.status(500).json({
+            error: err.message,
+          });
+        }
+        return res.json({
+          message: "Email has been sent",
+        });
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
+
+const ResetPassword = (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        res.status(400).send({ message: "Incorrect or Expired link" });
+      } else {
+        const { email } = decodedToken;
+        Users.findOne({
+          where: {
+            email: email,
+          },
+        })
+          .then((user) => {
+            if (!user) {
+              return res.status(404).send({ message: "User Not found." });
+            }
+            user
+              .update({
+                user_password: bcrypt.hashSync(password, 8),
+              })
+              .then(() => {
+                res.status(200).send({ message: "Password updated" });
+              })
+              .catch((err) => {
+                return res.status(500).send({ message: err.message });
+              });
+          })
+          .catch((err) => {
+            return res.status(500).send({ message: err.message });
+          });
+      }
+    });
+  } else {
+    return res.status(400).send({ message: "Something went wrong" });
+  }
+};
+
+export { signup, activate, login, SendResetPassword, ResetPassword };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,4 +1,10 @@
-import { signup, activate, login } from "../controllers/auth.controllers.js";
+import {
+  signup,
+  activate,
+  login,
+  SendResetPassword,
+  ResetPassword,
+} from "../controllers/auth.controllers.js";
 import { isLogin, isActivated } from "../middlewares/auth.middlewares.js";
 import { checkEmail } from "../middlewares/accountChecker.middlewares.js";
 import { isSuperAdmin } from "../middlewares/roles.middlewares.js";
@@ -14,6 +20,8 @@ const authRoutes = (app) => {
   router.post("/signup", checkEmail, signup);
   router.get("/activate/:token", activate);
   router.post("/login", login);
+  router.post("/reset-password", SendResetPassword);
+  router.post("/reset-password/:token", ResetPassword);
 
   app.use("/api/auth", router);
 };
