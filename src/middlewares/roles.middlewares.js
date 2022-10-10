507786fca505171a0e1f97a8dfdae5672f870736
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-10-10 11:25:39 +0700

    Fixing Middlewares

--- src/middlewares/roles.middlewares.js
+++ src/middlewares/roles.middlewares.js
@@ -6,72 +6,112 @@ const isSuperAdmin = (req, res, next) => {
   const email = req.email;
   const role = req.role;
 
-  if (role === 1) {
-    next();
+  if (role !== 1) {
+    return res.status(401).send({
+      message: "Token is not valid",
+      errorMessages: err.message,
+    });
   }
+
+  next();
 };
 
 const isAdminCS = (req, res, next) => {
   const email = req.email;
   const role = req.role;
 
-  if (role === 2) {
-    next();
+  if (role !== 2) {
+    return res.status(401).send({
+      message: "Token is not valid",
+      errorMessages: err.message,
+    });
   }
+
+  next();
 };
 
 const isAdminCashier = (req, res, next) => {
   const email = req.email;
   const role = req.role;
 
-  if (role === 3) {
-    next();
+  if (role !== 3) {
+    return res.status(401).send({
+      message: "Token is not valid",
+      errorMessages: err.message,
+    });
   }
+
+  next();
 };
 
 const isAdminDesign = (req, res, next) => {
   const email = req.email;
   const role = req.role;
 
-  if (role === 4) {
-    next();
+  if (role !== 4) {
+    return res.status(401).send({
+      message: "Token is not valid",
+      errorMessages: err.message,
+    });
   }
+
+  next();
 };
 
 const isAdminWarehouse = (req, res, next) => {
   const email = req.email;
   const role = req.role;
 
-  if (role === 5) {
-    next();
+  if (role !== 5) {
+    return res.status(401).send({
+      message: "Token is not valid",
+      errorMessages: err.message,
+    });
   }
+
+  next();
 };
 
 const isAdminProduction = (req, res, next) => {
   const email = req.email;
   const role = req.role;
 
-  if (role === 6) {
-    next();
+  if (role !== 6) {
+    return res.status(401).send({
+      message: "Token is not valid",
+      errorMessages: err.message,
+    });
   }
+
+  next();
 };
 
 const isAdminTu = (req, res, next) => {
   const email = req.email;
   const role = req.role;
 
-  if (role === 7) {
-    next();
+  if (role !== 7) {
+    return res.status(401).send({
+      message: "Token is not valid",
+      errorMessages: err.message,
+    });
   }
+
+  next();
 };
 
 const isUser = (req, res, next) => {
   const email = req.email;
   const role = req.role;
 
-  if (role === 8) {
-    next();
+  if (role !== 8) {
+    return res.status(401).send({
+      message: "Token is not valid",
+      errorMessages: err.message,
+    });
   }
+
+  next();
 };
 
 export { isSuperAdmin };
