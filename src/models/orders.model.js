2c70beee85e727357eff2f1b60473d1811e2d205
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-04 10:13:12 +0700

    Revisi Orders Model

--- src/models/orders.model.js
+++ src/models/orders.model.js
@@ -13,35 +13,23 @@ const Orders = (sequelize, Sequelize) => {
       order_user_id: {
         type: Sequelize.INTEGER,
       },
-      order_quantity: {
-        type: Sequelize.INTEGER,
-      },
-      order_status:{
+      order_status: {
         type: Sequelize.INTEGER,
       },
       order_note: {
         type: Sequelize.STRING,
       },
-      order_price: {
-        type: Sequelize.INTEGER,
-      },
       order_total_price: {
         type: Sequelize.INTEGER,
       },
-      order_finishing_id: {
+      order_discount: {
         type: Sequelize.INTEGER,
-      },
-      order_material_id: {
-        type: Sequelize.INTEGER,
-      },
-      order_design: {
-        type: Sequelize.STRING,
-      },
-      order_design_image:{
-        type: Sequelize.TEXT("long"),
       },
       order_payment_method: {
         type: Sequelize.STRING,
+      },
+      order_last_payment_date: {
+        type: Sequelize.DATE,
       },
       order_payment_status: {
         type: Sequelize.STRING,
