7943dd18f7210429365aaf3a8db9ee2f2f418a3d
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-06 15:52:19 +0700

    Edit API Docs Generator

--- swagger.js
+++ swagger.js
@@ -2,14 +2,14 @@ import swaggerAutogen from "swagger-autogen";
 
 const outputFile = "./docs/swagger_output.json";
 const endpointsFiles = [
-    "./src/routes/users.routes.js",
-    "./src/routes/order.routes.js",
-    "./src/routes/admin_cs.rotes.js",
-    "./src/routes/admin_kasir.routes.js",
-    "./src/routes/admin_produksi.routes.js",
-    "./src/routes/admin_tu.routes.js",
-    "./src/routes/admin_warehouse.routes.js",
-    "./src/routes/admin_desain.routes.js",
-    "./src/routes/super_admin.routes.js",
+  "./src/routes/users.routes.js",
+  "./src/routes/order.routes.js",
+  "./src/routes/admin_cs.routes.js",
+  "./src/routes/admin_kasir.routes.js",
+  "./src/routes/admin_produksi.routes.js",
+  "./src/routes/admin_tu.routes.js",
+  "./src/routes/admin_warehouse.routes.js",
+  "./src/routes/admin_desain.routes.js",
+  "./src/routes/super_admin.routes.js",
 ];
 swaggerAutogen(outputFile, endpointsFiles);
