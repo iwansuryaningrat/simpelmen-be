8f85447f12d9ceb285ebbfeba98b296526fe7f73
wahyuwidiwidayat <widiw598@gmail.com>
2022-12-13 15:25:08 +0700

    fix

--- src/controllers/admin/admin_design.controller.js
+++ src/controllers/admin/admin_design.controller.js
@@ -276,7 +276,7 @@ const RemoveDesain = (req, res) => {
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "../../../public/images");
+        cb(null, "./public/images");
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
